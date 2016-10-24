cbacdf7488cff19b86baa35d9aa47e15046161cd
Rafael Bedia <rbedia@harmonia.com>
2016-10-24 16:45:18 -0400

    Find all of the leaf groups the user is a member of.
    * This is specific to Active Directory and takes advantage of the
    LDAP_MATCHING_RULE_IN_CHAIN feature.

--- lib/Auth/Verify/LDAP.pm
+++ lib/Auth/Verify/LDAP.pm
@@ -29,18 +29,40 @@ sub _ldap_member_of_groups {
     $uid = escape_filter_value($uid);
     my $mail_attr = Bugzilla->params->{"LDAPmailattribute"};
     my $base_dn = Bugzilla->params->{"LDAPBaseDN"};
+
+    # Find the immediate groups the user is a member of.
     my $dn_result = $self->ldap->search(( base   => $base_dn,
                                           scope  => 'sub',
                                           filter => "$mail_attr=$uid" ),
-                                        attrs => ['memberof']);
+                                        attrs => ['memberof', 'distinguishedName']);
 
     if ($dn_result->code) {
         ThrowCodeError('ldap_search_error',
             { errstr => $dn_result->error, username => $uid });
     }
 
+    my @dns;
+    push @dns, $_->get_value('distinguishedName') for $dn_result->entries;
+    my $user_dn = @dns[0];
+    
     my @ldap_group_dns;
     push @ldap_group_dns, $_->get_value('memberof') for $dn_result->entries;
+
+    # Find all of the leaf groups the user is a member of.
+    # This is specific to Active Directory and it only works with DNs.
+    # 1.2.840.113556.1.4.1941 = LDAP_MATCHING_RULE_IN_CHAIN
+    # https://msdn.microsoft.com/en-us/library/aa746475(v=vs.85).aspx
+    my $groups_result = $self->ldap->search(( base   => $base_dn,
+                                          scope  => 'sub',
+                                          filter => "member:1.2.840.113556.1.4.1941:=$user_dn" ),
+                                        attrs => ['memberof']);
+
+    if ($groups_result->code) {
+        ThrowCodeError('ldap_search_error',
+            { errstr => $dn_result->error, username => $uid });
+    }
+
+    push @ldap_group_dns, $_->get_value('memberof') for $groups_result->entries;
 
     return \@ldap_group_dns;
 }
